0f8188efc47c4cefc516b3e64eecfcdc29a3c416
Nicolas TOURRETTE <nicotou1998@gmail.com>
2020-12-05 20:00:50 +0100

    Adding Front Main controller

---
+++ src/Controller/Front/MainFrontController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller\Front;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainFrontController extends AbstractController
+{
+	/**
+	 * @Route(path="/", name="front_home")
+	 */
+	public function index(){
+		return $this->render('base.html.twig');
+	}
+}
