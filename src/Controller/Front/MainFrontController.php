14cb9056ec410385b3e571c2c3384c18da9dbc18
Nicolas TOURRETTE <nicotou1998@gmail.com>
2020-12-06 21:41:43 +0100

    Adding some CSS contents and starting a web page template

--- src/Controller/Front/MainFrontController.php
+++ src/Controller/Front/MainFrontController.php
@@ -11,6 +11,8 @@ class MainFrontController extends AbstractController
 	 * @Route(path="/", name="front_home")
 	 */
 	public function index(){
-		return $this->render('base.html.twig');
+		return $this->render('base.html.twig', array(
+			"version" => "0.1.0"
+		));
 	}
 }
