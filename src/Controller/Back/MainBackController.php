fe5b06aa914a6d954497fb5e5da6f38d3197dfc1
Nicolas TOURRETTE <nicotou1998@gmail.com>
2020-12-16 20:56:06 +0100

    Starting backend layout for teachers

--- src/Controller/Back/MainBackController.php
+++ src/Controller/Back/MainBackController.php
@@ -2,20 +2,42 @@
 
 
 namespace App\Controller\Back;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class MainBackController
  * @package App\Controller\Back
- * @Route(path="/auth")
+ * @Route(path="/{space}", requirements={"space": "teacher|student|admin"})
  */
 class MainBackController extends AbstractController
 {
+	private function getPanelName(string $space): string
+	{
+		switch ($space) {
+			case "admin":
+				return "Administration";
+			case "teacher":
+				return "Enseignant";
+			case "student":
+				return "Étudiant";
+			default:
+				return "";
+		}
+	}
+	
 	/**
 	 * @Route(path="/", name="back_home")
+	 * @param string $space
+	 * @return Response
 	 */
-	public function index(){
-		return $this->render("back/pages/home.html.twig");
+	public function index(string $space): Response
+	{
+		return $this->render("back/" . $space . "/pages/home.html.twig", array(
+			"space" => $space,
+			"panel" => $this->getPanelName($space)
+		));
 	}
 }
--- src/Service/ServiceVersion.php
+++ src/Service/ServiceVersion.php
@@ -2,9 +2,24 @@
 
 namespace App\Service;
 
-class ServiceVersion {
-	public function getCurrentVersionNumber(): string {
+class ServiceVersion
+{
+	public function getCurrentVersionNumber(): string
+	{
 		$version = "0.1.0";
 		return $version;
+	}
+	
+	public function getBackofficeVersionNumber(string $backoffice): string
+	{
+		switch ($backoffice) {
+			case "admin":
+				return "undefined";
+			case "teacher":
+				return "0.1.0";
+			case "student":
+				return "undefined";
+			default: return "";
+		}
 	}
 }
